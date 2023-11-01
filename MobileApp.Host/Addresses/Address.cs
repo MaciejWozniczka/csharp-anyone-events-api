19517bbbc8e47da0a577cd804d78397de5789f99
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:07:40 +0100

    Added Address class

---
+++ MobileApp.Host/Addresses/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileApp.Host.Addresses;
+
+public class Address : BaseModel
+{
+    public Country Country { get; set; }
+    public string State { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+    public string Street { get; set; }
+    public string StreeNumber { get; set; }
+    public string LocalNumber { get; set; }
+}
