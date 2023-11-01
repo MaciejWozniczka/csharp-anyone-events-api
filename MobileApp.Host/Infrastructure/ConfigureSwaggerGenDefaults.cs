6d7666d1c560a7c50a0f2297ea6fdd9b6ce66ce8
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:54:06 +0100

    Added Swagger extension services

---
+++ MobileApp.Host/Infrastructure/ConfigureSwaggerGenDefaults.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileApp.Host.Infrastructure;
+
+public class ConfigureSwaggerGenDefaults : IConfigureOptions<SwaggerGenOptions>
+{
+    public void Configure(SwaggerGenOptions options)
+    {
+        options.OperationFilter<DefaultOperationIdFilter>();
+    }
+}
---
+++ MobileApp.Host/Infrastructure/DefaultOperationIdFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileApp.Host.Infrastructure;
+
+public class DefaultOperationIdFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        string actionName = ((ControllerActionDescriptor)context.ApiDescription.ActionDescriptor).ActionName;
+        operation.OperationId = actionName;
+    }
+}
