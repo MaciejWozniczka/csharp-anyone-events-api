8d6b1fd5e123cffe515f210a7cdb83248d9230c0
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:08:33 +0100

    Added Invoice class

---
+++ MobileApp.Host/Invoices/Invoice.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileApp.Host.Invoices;
+
+public class Invoice : BaseModel
+{
+    public Guid CompanyId { get; set; }
+    public Company Company { get; set; }
+    public string Number { get; set; }
+    public decimal Amount { get; set; }
+    public bool IsPaid { get; set; }
+}
