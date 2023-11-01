56877d7115147066db43c4bd3abed23830569942
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:08:23 +0100

    Added EventType class

---
+++ MobileApp.Host/EventTypes/EventType.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileApp.Host.EventTypes;
+
+public class EventType : BaseModel
+{
+    public Guid CategoryId { get; set; }
+    public Category Category { get; set; }
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Picture { get; set; }
+}
