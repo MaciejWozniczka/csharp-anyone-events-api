302bb681f597ca0dfb2e876adf3dd648930f9b3d
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:08:41 +0100

    Added Tenant class

---
+++ MobileApp.Host/Tenants/Tenant.cs
@@ -0,0 +1,15 @@
+﻿namespace MobileApp.Host.Tenants;
+
+public class Tenant : BaseModel
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Industry { get; set; }
+    public string TaxNumber { get; set; }
+    public string Logo { get; set; }
+    public Guid AddressId { get; set; }
+    public Address Address { get; set; }
+    public string UserId { get; set; }
+    public User User { get; set; }
+    public List<Company> Companies { get; set; }
+}
