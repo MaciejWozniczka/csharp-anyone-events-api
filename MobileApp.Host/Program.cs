5d6bad8b4b05891cb755d68591ceed063b8a99d8
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:56:22 +0100

    Updated Program class

--- MobileApp.Host/Program.cs
+++ MobileApp.Host/Program.cs
@@ -1,22 +1,103 @@
+using Hangfire;
+using Microsoft.AspNetCore.Diagnostics;
+using Serilog.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+var services = builder.Services;
+var configuration = builder.Configuration;
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.ClearProviders();
+    loggingBuilder.AddSerilog(dispose: true);
+});
+
+builder.Host.UseSerilog((host,
+    log) =>
+{
+    log.Enrich.FromLogContext();
+    log.MinimumLevel.Warning();
+    log.MinimumLevel.Override("TpaFinder", LogEventLevel.Information);
+    log.WriteTo.File(
+        Path.Combine("Logs", "log.txt"),
+        rollingInterval: RollingInterval.Day,
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] [X-Request-ID: {RequestId}] {Message:lj}{NewLine}{Exception}"
+    );
+    log.WriteTo.Console();
+});
+
+new MobileApp.Host.Module().GetServices(services, configuration);
+
+services.AddSpaStaticFiles(c =>
+{
+    c.RootPath = "ClientApp/dist";
+});
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    string requestId = context.TraceIdentifier;
+
+    using (LogContext.PushProperty("RequestId", requestId))
+    {
+        context.Response.Headers.Add("X-Request-ID", requestId);
+
+        await next.Invoke();
+    }
+});
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
+app.UseCors("default");
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseExceptionHandler(errorApp =>
+{
+    errorApp.Run(async context =>
+    {
+        var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+        var exception = exceptionHandlerFeature.Error;
+
+        Log.Logger.Error(exception, "Unhandled Exception");
+    });
+});
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapHangfireDashboard();
+});
+
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TpaFinder v1"));
+
+app.UseStaticFiles();
+app.UseSpaStaticFiles();
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "ClientApp";
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseProxyToSpaDevelopmentServer("http://localhost:5173/");
+    }
+});
+
+app.UseHangfireDashboard();
+app.UseHangfireServer(new BackgroundJobServerOptions());
+
+GlobalConfiguration.Configuration.UseSerilogLogProvider();
+
+BackgroundJob.Enqueue<ApplicationStart>(s => s.Start());
+
+new MobileApp.Host.Module().Run(app.Services);
 
 app.Run();
