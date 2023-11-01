7b8432d8e8970f417e6380059ea4b308c11bb517
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:53:47 +0100

    Added hangfire starter ApplicationStart

---
+++ MobileApp.Host/Infrastructure/ApplicationStart.cs
@@ -0,0 +1,15 @@
+﻿namespace MobileApp.Host.Infrastructure;
+
+public class ApplicationStart
+{
+    private readonly ILogger<ApplicationStart> _logger;
+    public ApplicationStart(ILogger<ApplicationStart> logger)
+    {
+        _logger = logger;
+    }
+
+    public void Start()
+    {
+        _logger.LogInformation("App started");
+    }
+}
