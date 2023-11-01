68539372131ce14a34d170a8d23f5f210111b64c
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:07:25 +0100

    Corrected User class

--- MobileApp.Host/Users/User.cs
+++ MobileApp.Host/Users/User.cs
@@ -5,15 +5,16 @@ public class User : IdentityUser
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public int Age { get; set; }
-    public Countries Country { get; set; }
+    public Country Country { get; set; }
     public string City { get; set; }
     public string Nationality { get; set; }
     public SexType Sex { get; set; }
     public List<string> Languages { get; set; }
-    public Guid? PictureId { get; set; }
+    public string Picture { get; set; }
     public string Desciption { get; set; }
     public int PhoneNumber { get; set; }
     public string PhoneCountryCode { get; set; }
+    public UserType UserType { get; set; }
     public DateTime CreateDate { get; set; }
     public bool IsDeleted { get; set; }
     public DateTime DeletingDate { get; set; }
