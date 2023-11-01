c73a57642ad0a10500c6b52cfd541a80524f5c8b
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:54:15 +0100

    Added UnitOfWorkBehavior

---
+++ MobileApp.Host/Infrastructure/UnitOfWorkBehavior.cs
@@ -0,0 +1,29 @@
+﻿namespace MobileApp.Host.Infrastructure;
+
+public class UnitOfWorkBehavior<TRequest, TResponse, TContext> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+    where TContext : DbContext
+{
+    private readonly TContext _db;
+    public UnitOfWorkBehavior(TContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        TResponse response = await next();
+
+        if (request.GetType().Name.EndsWith("Command"))
+        {
+            var result = response as Result;
+
+            if (result == null || result.Success)
+            {
+                await _db.SaveChangesAsync(cancellationToken);
+            }
+        }
+
+        return response;
+    }
+}
