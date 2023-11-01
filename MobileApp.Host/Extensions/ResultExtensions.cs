5daa0e266198e619394354f1421aa3d94a4fa53c
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:52:17 +0100

    Added Result class and extension

---
+++ MobileApp.Host/Extensions/ResultExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MobileApp.Host.Extensions;
+
+public static class ResultExtensions
+{
+    public static async Task<IActionResult> Process<T>(this Task<Result<T>> resultTask)
+    {
+        var result = await resultTask;
+        switch (result.Code)
+        {
+            case ResultCode.Ok:
+                return new OkObjectResult(result);
+            case ResultCode.BadRequest:
+                return new BadRequestObjectResult(result);
+            case ResultCode.NotFound:
+                return new NotFoundObjectResult(result);
+            case ResultCode.AccessDenied:
+                return new UnauthorizedResult();
+            case ResultCode.Forbidden:
+                return new ForbidResult();
+            case ResultCode.NoContent:
+                return new NoContentResult();
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
+}
---
+++ MobileApp.Host/Infrastructure/Result.cs
@@ -0,0 +1,193 @@
+﻿namespace MobileApp.Host.Infrastructure;
+
+public class Result
+{
+    public bool Success => Code == ResultCode.Ok;
+
+    public IEnumerable<ErrorMessage> Errors { get; set; }
+
+    [System.Text.Json.Serialization.JsonIgnore]
+    public ResultCode Code { get; set; }
+
+    public static Result Ok()
+    {
+        return new Result()
+        {
+            Code = ResultCode.Ok
+        };
+    }
+
+    public static Result<T> Ok<T>(T value)
+    {
+        return new Result<T>()
+        {
+            Value = value,
+            Code = ResultCode.Ok
+        };
+    }
+
+    public static Result BadRequest(string message)
+    {
+        var result = new Result
+        {
+            Code = ResultCode.BadRequest,
+            Errors = new List<ErrorMessage>()
+            {
+                new ErrorMessage()
+                {
+                    Message = message
+                }
+            }
+        };
+
+        return result;
+    }
+
+    public static Result BadRequest(IEnumerable<ErrorMessage> messages)
+    {
+        var result = new Result
+        {
+            Code = ResultCode.BadRequest,
+            Errors = messages
+        };
+
+        return result;
+    }
+
+    public static Result<T> BadRequest<T>(string message) where T : new()
+    {
+        var result = new Result<T>
+        {
+            Code = ResultCode.BadRequest,
+            Errors = new List<ErrorMessage>()
+            {
+                new ErrorMessage()
+                {
+                    Message = message
+                }
+            },
+            Value = new T()
+        };
+
+        return result;
+    }
+
+    public static Result<T> BadRequest<T>(IEnumerable<ErrorMessage> messages) where T : new()
+    {
+        var result = new Result<T>
+        {
+            Code = ResultCode.BadRequest,
+            Errors = messages,
+            Value = new T()
+        };
+
+        return result;
+    }
+    public static Result NotFound()
+    {
+        return new Result()
+        {
+            Code = ResultCode.NotFound,
+            Errors = new List<ErrorMessage>()
+            {
+                new ErrorMessage()
+                {
+                    Message = "There is no such object"
+                }
+            }
+        };
+    }
+
+    public static Result<T> NotFound<T>(Guid id) where T : new()
+    {
+        var result = new Result<T>
+        {
+            Code = ResultCode.NotFound,
+            Errors = new List<ErrorMessage>()
+            {
+                new ErrorMessage()
+                {
+                    Message = $"There is no object with the id {id}"
+                }
+            },
+            Value = new T()
+        };
+
+        return result;
+    }
+
+    public static Result<T> NotFound<T>(string message) where T : new()
+    {
+        var result = new Result<T>
+        {
+            Code = ResultCode.NotFound,
+            Errors = new List<ErrorMessage>()
+            {
+                new ErrorMessage()
+                {
+                    Message = $"There is no object {message}"
+                }
+            },
+            Value = new T()
+        };
+
+        return result;
+    }
+
+    public static Result<T> NotFound<T>() where T : new()
+    {
+        var result = new Result<T>
+        {
+            Code = ResultCode.NotFound,
+            Errors = new List<ErrorMessage>()
+            {
+                new ErrorMessage()
+                {
+                    Message = $"The requested object was not found"
+                }
+            },
+            Value = new T()
+        };
+
+        return result;
+    }
+
+    public static Result NotFound(Guid id)
+    {
+        var result = new Result
+        {
+            Code = ResultCode.NotFound,
+            Errors = new List<ErrorMessage>()
+            {
+                new ErrorMessage()
+                {
+                    Message = $"There is no object with the id {id}"
+                }
+            }
+        };
+
+        return result;
+    }
+}
+
+
+public class Result<T> : Result
+{
+    public T Value { get; set; }
+}
+
+public class ErrorMessage
+{
+    public string PropertyName { get; set; }
+    public string Message { get; set; }
+}
+
+public enum ResultCode
+{
+    Ok,
+    BadRequest,
+    NotFound,
+    AccessDenied,
+    Forbidden,
+    NoContent
+}
