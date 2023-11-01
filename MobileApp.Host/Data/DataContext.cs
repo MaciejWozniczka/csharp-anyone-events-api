8b9e96db8709045f5f36f66a4fc6b493bb809a63
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:13:57 +0100

    Separated DataContexts and added models

--- MobileApp.Host/Data/DataContext.cs
+++ MobileApp.Host/Data/DataContext.cs
@@ -1,14 +1,23 @@
 ﻿namespace MobileApp.Host.Data;
 
-public class DataContext : IdentityDbContext<User>
+public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
     }
 
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<EventType> EventTypes { get; set; }
+    public DbSet<Invoice> Invoices { get; set; }
+    public DbSet<Tenant> Tenants { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
     }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
     }
---
+++ MobileApp.Host/Data/DataContextUsers.cs
@@ -0,0 +1,16 @@
+﻿namespace MobileApp.Host.Data;
+
+public class DataContextUsers : IdentityDbContext<User>
+{
+    public DataContextUsers(DbContextOptions<DataContextUsers> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    }
+}
