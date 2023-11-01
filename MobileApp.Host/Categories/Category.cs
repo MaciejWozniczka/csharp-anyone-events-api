3440548646ace16765a0fd38dd5deaf2212288ee
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:07:55 +0100

    Added Category class

---
+++ MobileApp.Host/Categories/Category.cs
@@ -0,0 +1,12 @@
+﻿using MobileApp.Host.Events;
+
+namespace MobileApp.Host.Categories;
+
+public class Category : BaseModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Picture { get; set; }
+    public List<EventType> EventTypes { get; set; }
+    public List<Event> Events { get; set; }
+}
