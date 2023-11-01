f85392260514795e794f08602f660ea2ced46b77
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:55:40 +0100

    Added TokenOptions class

---
+++ MobileApp.Host/Infrastructure/TokenOption.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MobileApp.Host.Infrastructure;
+
+public class TokenOption
+{
+    [Required]
+    public string SecretKey { get; set; }
+    [Required]
+    public string Issuer { get; set; }
+    [Required]
+    public string Audience { get; set; }
+    [Required]
+    public int ExpiryMinutes { get; set; }
+}
