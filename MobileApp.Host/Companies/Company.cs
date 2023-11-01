2be2c9ccf3750efc77237d0005a738d01ecc828c
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:08:02 +0100

    Added Company class

---
+++ MobileApp.Host/Companies/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace MobileApp.Host.Companies;
+
+public class Company : BaseModel
+{
+    public Guid TenantId { get; set; }
+    public Tenant Tenant { get; set; }
+    public Guid AddressId { get; set; }
+    public Address Address { get; set; }
+    public string BankAccount { get; set; }
+    public string Picture { get; set; }
+}
