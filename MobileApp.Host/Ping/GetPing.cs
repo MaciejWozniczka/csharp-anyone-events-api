2e931b09cc756ea64fb1414cded65a7dc6c657fa
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:59:55 +0100

    Added GetPing controller

---
+++ MobileApp.Host/Ping/GetPing.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileApp.Host.Ping;
+
+[ApiController]
+public class GetPing : ControllerBase
+{
+    [SwaggerOperation(Tags = new[] { "Ping" }, Summary = "Check service status")]
+    [HttpGet("/api/ping")]
+    public IActionResult Ping()
+    {
+        return Ok("Pong");
+    }
+}
