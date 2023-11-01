96fb9428ecad6e10914f9c91323acc4234184ff1
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 14:10:01 +0100

    Added User class

---
+++ MobileApp.Host/Users/User.cs
@@ -0,0 +1,19 @@
+﻿using MobileApp.Host.Infrastructure;
+
+namespace MobileApp.Host.Users;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public Countries Country { get; set; }
+    public string City { get; set; }
+    public string Nationality { get; set; }
+    public SexType Sex { get; set; }
+    public List<string> Languages { get; set; }
+    public Guid? PictureId { get; set; }
+    public string Desciption { get; set; }
+    public int PhoneNumber { get; set; }
+    public string PhoneCountryCode { get; set; }
+}
