e39e96ae622ac276d1b9f602af0b83082222f4a9
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:52:26 +0100

    Added SerializationExtension

---
+++ MobileApp.Host/Extensions/SerializationExtension.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace MobileApp.Host.Extensions;
+
+public class SerializationExtension
+{
+    public static string SerializeWithoutNulls<T>(T model)
+    {
+        return JsonConvert.SerializeObject(model,
+            Formatting.None,
+            new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+    }
+}
