25373332d6f2e9190cd3939d3c645255f8095ae3
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:51:54 +0100

    Added ObjectExtensions

---
+++ MobileApp.Host/Extensions/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace MobileApp.Host.Extensions;
+
+public static class ObjectExtensions
+{
+    public static T Set<T>(this T obj, Action<T> action)
+    {
+        action(obj);
+        return obj;
+    }
+
+    public static bool IsOfType<T>(this object value)
+    {
+        if (value == null)
+        {
+            return false;
+        }
+
+        return value.GetType() == typeof(T);
+    }
+}
