b93f0f4fff4cf5cac0eaee3b354492e9fd3ed09d
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 16:52:35 +0100

    Added ValidationResultExtensions

---
+++ MobileApp.Host/Extensions/ValidationResultExtensions.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation.Results;
+
+namespace MobileApp.Host.Extensions;
+
+public static class ValidationResultExtensions
+{
+    public static Result<T> ToResult<T>(this ValidationResult validationResult)
+    {
+        var result = new Result<T>
+        {
+            Code = ResultCode.BadRequest,
+            Errors = new List<ErrorMessage>()
+            {
+                new ErrorMessage()
+                {
+                    Message = validationResult.Errors.First().ErrorMessage
+                }
+            }
+        };
+
+        return result;
+    }
+}
