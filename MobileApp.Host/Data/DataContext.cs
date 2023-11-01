de3d601a9f0950c7dcfa51ec5bc492dfa1136706
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-01 14:11:38 +0100

    Added DataContext with User class

---
+++ MobileApp.Host/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace MobileApp.Host.Data;
+
+public class DataContext : IdentityDbContext<User>
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+    }
+}
