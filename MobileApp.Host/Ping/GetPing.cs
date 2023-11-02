ce96c65d6e6bc68a97db145e2fe972dcd4823b06
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-02 16:51:52 +0100

    Added logger to GetPing

--- MobileApp.Host/Ping/GetPing.cs
+++ MobileApp.Host/Ping/GetPing.cs
@@ -3,10 +3,17 @@
 [ApiController]
 public class GetPing : ControllerBase
 {
+    private readonly ILogger<GetPing> _logger;
+    public GetPing(ILogger<GetPing> logger)
+    {
+        _logger = logger;
+    }
+
     [SwaggerOperation(Tags = new[] { "Ping" }, Summary = "Check service status")]
     [HttpGet("/api/ping")]
     public IActionResult Ping()
     {
+        _logger.LogInformation("Service status check");
         return Ok("Pong");
     }
 }
