ed4c7f7f6fb94cdf70787bebb4a054fdd378cdb9
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-23 00:46:01 +0100

    Corrected Address model

--- MobileApp.Host/Addresses/Address.cs
+++ MobileApp.Host/Addresses/Address.cs
@@ -2,11 +2,16 @@
 
 public class Address : BaseModel
 {
-    public Country Country { get; set; }
+    public string Label { get; set; }
+    public string CountryCode { get; set; }
+    public string CountryName { get; set; }
+    public string StateCode { get; set; }
     public string State { get; set; }
+    public string CountyCode { get; set; }
+    public string County { get; set; }
     public string City { get; set; }
-    public string PostalCode { get; set; }
+    public string District { get; set; }
     public string Street { get; set; }
-    public string StreeNumber { get; set; }
-    public string ApartmentNumber { get; set; }
+    public string PostalCode { get; set; }
+    public string HouseNumber { get; set; }
 }
