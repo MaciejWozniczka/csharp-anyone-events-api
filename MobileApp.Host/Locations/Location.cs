93625f5eb0d93dfb6d325edf39521fcd897c09c9
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-22 17:12:15 +0100

    Migration to db

--- MobileApp.Host/Locations/Location.cs
+++ MobileApp.Host/Locations/Location.cs
@@ -4,4 +4,5 @@ public class Location : BaseModel
 {
     public double Latitude { get; set; }
     public double Longitude { get; set; }
+    public int Distance { get; set; }
 }
---
+++ MobileApp.Host/Migrations/20231122160850_NameDeletedFromEvent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MobileApp.Host.Migrations
+{
+    public partial class NameDeletedFromEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Events");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Distance",
+                table: "Location",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Distance",
+                table: "Location");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
