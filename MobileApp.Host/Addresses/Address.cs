65561b904478ae0d04f0089440024d3cb2d27c33
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-24 00:27:05 +0100

    Nullable parameters in HereGeocode class

--- MobileApp.Host/Addresses/Address.cs
+++ MobileApp.Host/Addresses/Address.cs
@@ -2,17 +2,17 @@
 
 public class Address : BaseModel
 {
-    public string Label { get; set; }
-    public string CountryCode { get; set; }
-    public string CountryName { get; set; }
-    public string StateCode { get; set; }
-    public string State { get; set; }
-    public string CountyCode { get; set; }
-    public string County { get; set; }
-    public string City { get; set; }
-    public string District { get; set; }
-    public string Street { get; set; }
-    public string PostalCode { get; set; }
-    public string HouseNumber { get; set; }
+    public string? Label { get; set; }
+    public string? CountryCode { get; set; }
+    public string? CountryName { get; set; }
+    public string? StateCode { get; set; }
+    public string? State { get; set; }
+    public string? CountyCode { get; set; }
+    public string? County { get; set; }
+    public string? City { get; set; }
+    public string? District { get; set; }
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+    public string? HouseNumber { get; set; }
     public string? ApartmentNumber { get; set; }
 }
