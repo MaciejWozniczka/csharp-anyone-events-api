5cd164f71504deecdf0ce8a29d442e32cfadb388
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-23 22:05:56 +0100

    Address migration to db

---
+++ MobileApp.Host/Migrations/20231123181631_AddressChange.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MobileApp.Host.Migrations
+{
+    public partial class AddressChange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Addresses");
+
+            migrationBuilder.RenameColumn(
+                name: "StreeNumber",
+                table: "Addresses",
+                newName: "StateCode");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApartmentNumber",
+                table: "Addresses",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryCode",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountryName",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "County",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CountyCode",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "District",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HouseNumber",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Label",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CountryCode",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "CountryName",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "County",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "CountyCode",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "District",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "HouseNumber",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Label",
+                table: "Addresses");
+
+            migrationBuilder.RenameColumn(
+                name: "StateCode",
+                table: "Addresses",
+                newName: "StreeNumber");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ApartmentNumber",
+                table: "Addresses",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Country",
+                table: "Addresses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
