317fec8e1f072c15576c004d7bc80b211ca791e8
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-11-30 09:23:06 +0100

    Added Communication model

---
+++ MobileApp.Host/Communications/Communication.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileApp.Host.Communications;
+
+public class Communication : BaseModel
+{
+    public Guid? EventId { get; set; }
+    [JsonIgnore]
+    public UserEvent? Event { get; set; }
+    public string? Message { get; set; }
+    public string? UserId { get; set; }
+    [JsonIgnore]
+    public User? User { get; set; }
+}
