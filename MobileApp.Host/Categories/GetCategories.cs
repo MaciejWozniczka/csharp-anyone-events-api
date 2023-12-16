e5d9a57848acf83ed4c9f771df43021f170f3716
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2023-12-16 02:54:36 +0100

    Corrected GetCategories controller

--- MobileApp.Host/Categories/GetCategories.cs
+++ MobileApp.Host/Categories/GetCategories.cs
@@ -23,9 +23,10 @@ public class GetCategories : ControllerBase
 
     public class GetCategoriesDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string Picture { get; set; }
+        public string? Description { get; set; }
+        public string? Picture { get; set; }
     }
 
     public class MapperProfile : Profile
