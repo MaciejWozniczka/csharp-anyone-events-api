f99eb730c2b946b2b9b950681c58c7ec8706578a
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-01-11 17:31:54 +0100

    Migration to db

--- MobileApp.Host/Migrations/20240111161529_UserFilterDateTimeFromTo.Designer.cs
+++ MobileApp.Host/Migrations/20240111161529_UserFilterDateTimeFromTo.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MobileApp.Host.Data;
 
@@ -11,9 +12,10 @@ using MobileApp.Host.Data;
 namespace MobileApp.Host.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240111161529_UserFilterDateTimeFromTo")]
+    partial class UserFilterDateTimeFromTo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
