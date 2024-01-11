75793ce91e0b36915292189cc2a6cd68754419ec
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-01-11 18:29:10 +0100

    Added DeleteEvent controller

---
+++ MobileApp.Host/Events/DeleteEvent.cs
@@ -0,0 +1,52 @@
+﻿namespace MobileApp.Host.Events;
+
+[ApiController]
+public class DeleteEvent : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public DeleteEvent(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [Authorize]
+    [SwaggerOperation(Tags = new[] { "Events" }, Summary = "Change event status to deleted")]
+    [HttpDelete("/api/event/{id}")]
+    public async Task<Result> DeleteEventAsync(Guid id)
+    {
+        return await _mediator.Send(new DeleteEventCommand(id));
+    }
+
+    public class DeleteEventCommand : IRequest<Result>
+    {
+        public Guid Id { get; set; }
+        public DeleteEventCommand(Guid id)
+        {
+            Id = id;
+        }
+    }
+
+    public class DeleteEventCommandHandler : IRequestHandler<DeleteEventCommand, Result>
+    {
+        private readonly DataContext _db;
+        public DeleteEventCommandHandler(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Result> Handle(DeleteEventCommand request, CancellationToken cancellationToken)
+        {
+            var userEvent = await _db.Categories
+                .FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+
+            if (userEvent == null)
+            {
+                return Result.NotFound<Guid>(request.Id);
+            }
+
+            userEvent.IsDeleted = true;
+
+            return Result.Ok();
+        }
+    }
+}
