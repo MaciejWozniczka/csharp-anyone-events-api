13f093fd5a05fc09ffc19a1a8b2aa9cb80ff6c85
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-01-11 18:19:42 +0100

    Added GetCurrentUser controller

---
+++ MobileApp.Host/Users/GetCurrentUser.cs
@@ -0,0 +1,76 @@
+﻿using static MobileApp.Host.EventTypes.GetEventTypesByCategoryId;
+
+namespace MobileApp.Host.Users;
+
+[ApiController]
+public class GetCurrentUser : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public GetCurrentUser(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [Authorize]
+    [SwaggerOperation(Tags = new[] { "User" }, Summary = "Get current user")]
+    [HttpGet("/api/user/")]
+    public async Task<Result<GetCurrentUserDto>> GetCurrentUserAsync()
+    {
+        return await _mediator.Send(new GetCurrentUserQuery());
+    }
+
+    public class GetCurrentUserQuery : IRequest<Result<GetCurrentUserDto>>
+    {
+    }
+
+    public class GetCurrentUserDto
+    {
+        public string? Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? Age { get; set; }
+        public Country? Country { get; set; }
+        public SexType? Sex { get; set; }
+        public string? Picture { get; set; }
+        public string? Desciption { get; set; }
+        public int? PhoneNumber { get; set; }
+        public string? PhoneCountryCode { get; set; }
+        public UserType? UserType { get; set; }
+    }
+
+    public class GetCurrentUserQueryHandler : IRequestHandler<GetCurrentUserQuery, Result<GetCurrentUserDto>>
+    {
+        private readonly ICurrentUserAccessor _currentUserAccessor;
+        public GetCurrentUserQueryHandler(ICurrentUserAccessor currentUserAccessor)
+        {
+            _currentUserAccessor = currentUserAccessor;
+        }
+
+        public async Task<Result<GetCurrentUserDto>> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _currentUserAccessor.GetCurrentUser();
+
+            if (user == null)
+            {
+                return Result.NotFound<GetCurrentUserDto>("User not found");
+            }
+
+            var result = new GetCurrentUserDto()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Age = user.Age,
+                Country = user.Country,
+                Sex = user.Sex,
+                Picture = user.Picture,
+                Desciption = user.Desciption,
+                PhoneNumber = user.PhoneNumber,
+                PhoneCountryCode = user.PhoneCountryCode,
+                UserType = user.UserType
+            };
+
+            return Result.Ok(result);
+        }
+    }
+}
