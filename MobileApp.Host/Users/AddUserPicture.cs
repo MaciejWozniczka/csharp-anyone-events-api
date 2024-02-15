1f6260c28c5b533f641e148c6423396979ee61f2
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-02-15 18:32:34 +0100

    Added AddUserPicture controller

---
+++ MobileApp.Host/Users/AddUserPicture.cs
@@ -0,0 +1,55 @@
+﻿namespace MobileApp.Host.Users;
+
+[ApiController]
+public class AddUserPicture : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AddUserPicture(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [Authorize]
+    [SwaggerOperation(Tags = new[] { "User" }, Summary = "Add user picture")]
+    [HttpPost("/api/user/picture")]
+    public async Task<Result<string>> Import(IFormFile file)
+    {
+        return await _mediator.Send(new AddUserPictureCommand() { DataFile = file });
+    }
+
+    public class AddUserPictureCommand : IRequest<Result<string>>
+    {
+        [JsonIgnore]
+        public IFormFile? DataFile { get; set; }
+    }
+
+    public class AddUserPictureHandler : IRequestHandler<AddUserPictureCommand, Result<string>>
+    {
+        private readonly DataContext _db;
+        private readonly ICurrentUserAccessor _currentUserAccessor;
+        public AddUserPictureHandler(DataContext db, ICurrentUserAccessor currentUserAccessor)
+        {
+            _db = db;
+            _currentUserAccessor = currentUserAccessor;
+        }
+
+        public async Task<Result<string>> Handle(AddUserPictureCommand request, CancellationToken cancellationToken)
+        {
+            var currentUser = await _currentUserAccessor.GetCurrentUser();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                request.DataFile.CopyTo(memoryStream);
+                var bytes = memoryStream.ToArray();
+                var content = Convert.ToBase64String(bytes);
+                currentUser.Picture = content;
+            }
+
+            _db.Update(currentUser);
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return Result.Ok(currentUser.Id);
+        }
+    }
+}
