1ce3af454f9bab45f4faef1c561a7b5bf33008e5
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-02-15 18:32:45 +0100

    Added DeleteUserPicture controller

---
+++ MobileApp.Host/Users/DeleteUserPicture.cs
@@ -0,0 +1,49 @@
+﻿namespace MobileApp.Host.Users;
+
+[ApiController]
+public class DeleteUserPicture : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public DeleteUserPicture(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [Authorize]
+    [SwaggerOperation(Tags = new[] { "User" }, Summary = "Add user picture")]
+    [HttpPost("/api/user/picture")]
+    public async Task<Result<string>> Import(IFormFile file)
+    {
+        return await _mediator.Send(new DeleteUserPictureCommand() { DataFile = file });
+    }
+
+    public class DeleteUserPictureCommand : IRequest<Result<string>>
+    {
+        [JsonIgnore]
+        public IFormFile? DataFile { get; set; }
+    }
+
+    public class DeleteUserPictureHandler : IRequestHandler<DeleteUserPictureCommand, Result<string>>
+    {
+        private readonly DataContext _db;
+        private readonly ICurrentUserAccessor _currentUserAccessor;
+        public DeleteUserPictureHandler(DataContext db, ICurrentUserAccessor currentUserAccessor)
+        {
+            _db = db;
+            _currentUserAccessor = currentUserAccessor;
+        }
+
+        public async Task<Result<string>> Handle(DeleteUserPictureCommand request, CancellationToken cancellationToken)
+        {
+            var currentUser = await _currentUserAccessor.GetCurrentUser();
+
+            currentUser.Picture = null;
+
+            _db.Update(currentUser);
+            await _db.SaveChangesAsync(cancellationToken);
+
+            return Result.Ok(currentUser.Id);
+        }
+    }
+}
