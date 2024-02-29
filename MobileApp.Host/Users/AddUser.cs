0d008cbc5b3c54fa31a5c3518bbe6ec0a30b27c1
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-02-29 18:43:23 +0100

    Adding users without auth

--- MobileApp.Host/Users/AddUser.cs
+++ MobileApp.Host/Users/AddUser.cs
@@ -9,7 +9,6 @@ public class AddUser : ControllerBase
         _mediator = mediator;
     }
 
-    [Authorize]
     [SwaggerOperation(Tags = new[] { "Users" }, Summary = "Add user")]
     [HttpPost("/api/user")]
     public async Task<Result> AddUserAsync([FromBody] AddUserQuery addUserRequestBody)
