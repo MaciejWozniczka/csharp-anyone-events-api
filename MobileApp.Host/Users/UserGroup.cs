b5ddc27474287875a7a6b4a99eba04cac749873b
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-08-17 14:15:25 +0200

    Added BaseModel to PendingUser model

--- MobileApp.Host/Users/UserGroup.cs
+++ MobileApp.Host/Users/UserGroup.cs
@@ -7,7 +7,7 @@ public class UserGroup : BaseModel
     public bool IsVisible { get; set; } = false;
 }
 
-public class PendingUser
+public class PendingUser : BaseModel
 {
     public string UserId { get; set; }
     public bool Accepted { get; set; } = false;
