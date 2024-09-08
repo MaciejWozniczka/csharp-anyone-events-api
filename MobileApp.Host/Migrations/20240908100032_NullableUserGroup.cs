9ba19cf10fcb64953f26b3b2e7b2128c5bd4f38b
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-09-08 12:18:33 +0200

    Migration to db

---
+++ MobileApp.Host/Migrations/20240908100032_NullableUserGroup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MobileApp.Host.Migrations
+{
+    public partial class NullableUserGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Accepted",
+                table: "PendingUser",
+                type: "boolean",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "boolean");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "Accepted",
+                table: "PendingUser",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "boolean",
+                oldNullable: true);
+        }
+    }
+}
