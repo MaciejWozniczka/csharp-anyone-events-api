61eb1ca7e7d590a5ab308a6768e304e0769f50bd
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2024-11-17 15:49:35 +0100

    User BirthdayYear parameter added to User class

---
+++ MobileApp.Host/Migrations/20241117144905_UserBirhdayYear.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MobileApp.Host.Migrations
+{
+    public partial class UserBirhdayYear : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "AspNetUsers",
+                newName: "BirthdayYear");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BirthdayYear",
+                table: "AspNetUsers",
+                newName: "Age");
+        }
+    }
+}
