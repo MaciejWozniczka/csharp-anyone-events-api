401dfb07b6a8ad5b00465ac3c642a3429e54b7bc
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-01-19 13:13:13 +0100

    Added EmojiCode to EventType

--- MobileApp.Host/EventTypes/EventType.cs
+++ MobileApp.Host/EventTypes/EventType.cs
@@ -5,6 +5,6 @@ public class EventType : BaseModel
     public Guid CategoryId { get; set; }
     public Category Category { get; set; }
     public string Name { get; set; }
-    public string? Type { get; set; }
+    public string EmojiCode { get; set; }
     public string? Picture { get; set; }
 }
