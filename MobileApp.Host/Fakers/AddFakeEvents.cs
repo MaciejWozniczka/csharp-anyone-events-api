cb1e7ad9e2e639112c5b3f44982686c10a0b8949
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-01-19 13:14:40 +0100

    Added AddFakeEvents controller

---
+++ MobileApp.Host/Fakers/AddFakeEvents.cs
@@ -0,0 +1,38 @@
+﻿namespace MobileApp.Host.Fakers;
+
+[ApiController]
+public class AddFakeEvents : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public AddFakeEvents(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [SwaggerOperation(Tags = new[] { "Faker" }, Summary = "Add fake services")]
+    [HttpPost("/api/events/fake")]
+    public async Task<Result> AddFakeEventsAsync([FromBody] AddFakeEventsQuery addFakeEventsRequestBody)
+    {
+        return await _mediator.Send(new AddFakeEventsQuery());
+    }
+
+    public class AddFakeEventsQuery : IRequest<Result>
+    {
+    }
+
+    public class AddFakeEventsQueryHandler : IRequestHandler<AddFakeEventsQuery, Result>
+    {
+        private readonly IFakerService _fakerService;
+        public AddFakeEventsQueryHandler(IFakerService fakerService)
+        {
+            _fakerService = fakerService;
+        }
+
+        public async Task<Result> Handle(AddFakeEventsQuery request, CancellationToken cancellationToken)
+        {
+            await _fakerService.CreateFakeEvents(cancellationToken);
+
+            return Result.Ok();
+        }
+    }
+}
