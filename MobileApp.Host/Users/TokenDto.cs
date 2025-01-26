e0c903b5c46cdceae4d8d4262c3adb59e66f9ab3
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-01-26 12:35:26 +0100

    Added Id to TokenDto

--- MobileApp.Host/Users/TokenDto.cs
+++ MobileApp.Host/Users/TokenDto.cs
@@ -5,4 +5,5 @@ public class TokenDto
     public string AccessToken { get; set; }
     public DateTimeOffset AccessTokenExpiry { get; set; }
     public string RefreshToken { get; set; }
+    public string Id { get; set; }
 }
