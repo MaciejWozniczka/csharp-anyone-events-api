67921b9ade216d317d2557cdf668e0befa4d9580
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-04-05 12:46:33 +0200

    Updated ChatHub

--- MobileApp.Host/Messages/ChatHub.cs
+++ MobileApp.Host/Messages/ChatHub.cs
@@ -1,20 +1,19 @@
-﻿using Microsoft.AspNetCore.SignalR;
-
-namespace MobileApp.Host.Messages;
+﻿namespace MobileApp.Host.Messages;
 
 public interface IChatHub
 {
-    Task SendMessage(string roomName, string messageText);
+    Task SendMessage(Guid chatId, string messageText);
 }
 public class ChatHub(DataContext db, ICurrentUserAccessor currentUserAccessor) : Hub, IChatHub
 {
-    public async Task SendMessage(string roomName, string messageText)
+    public async Task SendMessage(Guid chatId, string messageText)
     {
         var currentUser = await currentUserAccessor.GetCurrentUser();
 
         var message = new Message
         {
-            RoomId = roomName,
+            RoomId = chatId.ToString(),
+            ChatId = chatId,
             SenderUserId = currentUser.Id,
             SenderUsername = currentUser.FirstName,
             Text = messageText
@@ -23,6 +22,6 @@ public class ChatHub(DataContext db, ICurrentUserAccessor currentUserAccessor) :
         db.Messages.Add(message);
         await db.SaveChangesAsync();
 
-        await Clients.Group(roomName).SendAsync("ReceiveMessage", message.SenderUsername, messageText);
+        await Clients.Group(message.RoomId).SendAsync("ReceiveMessage", message.SenderUsername, messageText);
     }
 }
