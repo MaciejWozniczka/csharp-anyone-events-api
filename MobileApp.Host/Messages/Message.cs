5dda403b4fa45c14ce8fc0ab8268573414b9093d
Maciej Woźniczka <maciej.wozniczka@tpa-group.pl>
2025-04-05 12:46:50 +0200

    Added Chat parameter to Message class

--- MobileApp.Host/Messages/Message.cs
+++ MobileApp.Host/Messages/Message.cs
@@ -1,10 +1,13 @@
-﻿namespace MobileApp.Host.Messages;
+﻿using MobileApp.Host.Chats;
+
+namespace MobileApp.Host.Messages;
 
 public class Message : BaseModel
 {
     public string RoomId { get; set; }
-    public string ChatName { get; set; }
     public string SenderUserId { get; set; }
     public string SenderUsername { get; set; }
     public string Text { get; set; }
+    public Guid ChatId { get; set; }
+    public Chat Chat { get; set; }
 }
